1e015fdaeb82e4b53f00316e1492a65e0aaf1c9d
omer <oogur348@gmail.com>
2022-09-29 16:31:50 +0300

    css simple example

---
+++ src/css-calisma/travel-page/index.js
@@ -0,0 +1,15 @@
+const isim = "omer"
+
+function myfunc() {
+    if(window.pageYOffset>150){
+        document.getElementById("menu").classList.add("menu-scroll")
+    }else{
+        document.getElementById("menu").classList.remove("menu-scroll")
+    }
+}
+
+
+
+
+console.log({isim});
+
